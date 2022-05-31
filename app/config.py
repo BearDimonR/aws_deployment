b1866b64ad0ab77e350d7c255099c3e36df874da
BeardimonR <beardimon2001@gmail.com>
2022-05-31 21:14:00 +0300

    conver port to number

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
 host = os.environ['MYSQL_HOST']
 database = os.environ['MYSQL_DB']
-port = os.environ['MYSQL_PORT']
+port = int(os.environ['MYSQL_PORT'])
 
 DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
