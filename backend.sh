fd45c01173df40c654a64496ca86b59bc18b35b9
BeardimonR <beardimon2001@gmail.com>
2022-05-31 21:51:04 +0300

    fix backend service

--- backend.sh
+++ backend.sh
@@ -8,7 +8,6 @@ export MYSQL_HOST=$MYSQL_HOST
 export MYSQL_DB=$MYSQL_DB
 export MYSQL_PORT=$MYSQL_PORT
 
-echo "mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_PORT}"
-
 cd app
-venv/bin/gunicorn -b localhost:8000 app:app
+source venv/bin/activate
+gunicorn -b localhost:8000 app:app
