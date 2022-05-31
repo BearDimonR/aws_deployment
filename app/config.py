4f9911f7e1cccc725eb6a3841959206f0d8acad9
BeardimonR <beardimon2001@gmail.com>
2022-05-31 21:18:33 +0300

    fix backend starter

--- app/config.py
+++ app/config.py
@@ -5,10 +5,6 @@ user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
 host = os.environ['MYSQL_HOST']
 database = os.environ['MYSQL_DB']
-port = int(os.environ['MYSQL_PORT'])
+port = os.environ['MYSQL_PORT']
 
-print(port)
-
-DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{str(port)}/{database}'
-
-print(DATABASE_CONNECTION_URI)
+DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
--- backend.sh
+++ backend.sh
@@ -3,10 +3,11 @@
 . .env
 
 export MYSQL_USER=$MYSQL_USER
-export MYSQL_PASSWORD=$MYSQL_USER
-export MYSQL_HOST=$MYSQL_USER
-export MYSQL_DB=$MYSQL_USER
-export MYSQL_PORT=$MYSQL_USER
+export MYSQL_PASSWORD=$MYSQL_PASSWORD
+export MYSQL_HOST=$MYSQL_HOST
+export MYSQL_DB=$MYSQL_DB
+export MYSQL_PORT=$MYSQL_PORT
+
 
 cd app
 venv/bin/gunicorn -b localhost:8000 app:app
