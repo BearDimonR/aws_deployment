78d4f440de8e441ec8d847e70304304ca31593ec
BeardimonR <beardimon2001@gmail.com>
2022-05-31 21:14:57 +0300

    convert port to string

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ host = os.environ['MYSQL_HOST']
 database = os.environ['MYSQL_DB']
 port = int(os.environ['MYSQL_PORT'])
 
-DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
+DATABASE_CONNECTION_URI = f'mysql+pymysql://{user}:{password}@{host}:{str(port)}/{database}'
